73658c4445e3c8729528d97b86ec92804ebd29e5
DongJo Kim <dong.jo.kim@cern.ch>
2023-01-07 13:06:35 +0200

    a working version

--- macros/ML/z02.trainData.py
+++ macros/ML/z02.trainData.py
@@ -16,7 +16,7 @@ print('We have {} events'.format(len(data)))
 
 # the data coming out of previous commands is a list of 2D arrays. We want a 3D np array (n_events, xpixels, ypixels)
 x_data = np.vstack(data[:])
-y_data = np.vstack(dataTrue[:]) 
+y_data = np.array([0]*300)
 
 print(x_data.shape, y_data.shape)
 
@@ -37,50 +37,49 @@ print('We will train+validate on {0} images, leaving {1} for cross-validation'.f
 
 model0 = Sequential()
 model0.add(Flatten(input_shape=(32,32,1)))  # Images are a 3D matrix, we have to flatten them to be 1D
-model0.add(Dense(1, kernel_initializer='normal', activation='softmax'))
+model0.add(Dense(2, kernel_initializer='normal', activation='softmax'))
 
 # Compile model
 model0.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#sparse_categorical_crossentropy
 
 history_logi = model0.fit(x_train, y_train, validation_split=0.2, epochs=40, batch_size=100, shuffle=True, verbose=0)
-# model1 = Sequential()
-# model1.add(Flatten(input_shape=(16, 16, 1)))  # Images are a 3D matrix, we have to flatten them to be 1D
-# model1.add(Dense(100, kernel_initializer='normal', activation='tanh'))
-# model1.add(Dropout(0.5)) # drop a unit with  50% probability.
+model1 = Sequential()
+model1.add(Flatten(input_shape=(32, 32, 1)))  # Images are a 3D matrix, we have to flatten them to be 1D
+model1.add(Dense(100, kernel_initializer='normal', activation='tanh'))
+model1.add(Dropout(0.5)) # drop a unit with  50% probability.
 
-# model1.add(Dense(100, kernel_initializer='orthogonal',activation='tanh'))
-# model1.add(Dropout(0.5)) # drop a unit with  50% probability.
+model1.add(Dense(100, kernel_initializer='orthogonal',activation='tanh'))
+model1.add(Dropout(0.5)) # drop a unit with  50% probability.
 
-# model1.add(Dense(100, kernel_initializer='orthogonal',activation='tanh'))
-# # model.add(Activation('sigmoid'))
-# model1.add(Dense(2, kernel_initializer='normal', activation='softmax')) # last layer, this has a softmax to do the classification
+model1.add(Dense(100, kernel_initializer='orthogonal',activation='tanh'))
+# model.add(Activation('sigmoid'))
+model1.add(Dense(2, kernel_initializer='normal', activation='softmax')) # last layer, this has a softmax to do the classification
 
-# model1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model1.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# history_mlp = model1.fit(x_train, y_train, validation_split=0.2, epochs=40, batch_size=100, shuffle=True, verbose=1)
-# model_cnn = Sequential()
-# model_cnn.add(Conv2D(32, (3, 3), input_shape=(16, 22, 1), activation='relu'))
-# model_cnn.add(Conv2D(32, (3, 3), activation='relu'))
-# model_cnn.add(MaxPooling2D(pool_size=(2, 2)))
-# model_cnn.add(Dropout(0.25))
+history_mlp = model1.fit(x_train, y_train, validation_split=0.2, epochs=40, batch_size=100, shuffle=True, verbose=1)
+model_cnn = Sequential()
+model_cnn.add(Conv2D(32, (3, 3), input_shape=(32, 32, 1), activation='relu'))
+model_cnn.add(Conv2D(32, (3, 3), activation='relu'))
+model_cnn.add(MaxPooling2D(pool_size=(2, 2)))
+model_cnn.add(Dropout(0.25))
 
-# model_cnn.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-# model_cnn.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-# model_cnn.add(MaxPooling2D(pool_size=(2, 2)))
-# model_cnn.add(Dropout(0.25))
+model_cnn.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model_cnn.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model_cnn.add(MaxPooling2D(pool_size=(2, 2)))
+model_cnn.add(Dropout(0.25))
 
-# model_cnn.add(Flatten())
-# model_cnn.add(Dense(300, activation='relu'))
-# model_cnn.add(Dropout(0.5))
-# model_cnn.add(Dense(2, activation='softmax'))
+model_cnn.add(Flatten())
+model_cnn.add(Dense(300, activation='relu'))
+model_cnn.add(Dropout(0.5))
+model_cnn.add(Dense(2, activation='softmax'))
 
-# # Compile model
-# model_cnn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# history_cnn = model_cnn.fit(x_train, y_train, validation_split=0.2, epochs=40, batch_size=100, shuffle=True, verbose=1)
-# model_dir='trained_models/'
-# if not os.path.isdir(model_dir): os.system('mkdir '+model_dir)
-# model0.save(model_dir+'logi.h5')
-# model1.save(model_dir+'mlp.h5')
-# model_cnn.save(model_dir+'cnn.h5')
-# np.savez(model_dir+'training_histories.npz', [ history.history for history in [history_logi, history_mlp, history_cnn ]])
+# Compile model
+model_cnn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+history_cnn = model_cnn.fit(x_train, y_train, validation_split=0.2, epochs=40, batch_size=100, shuffle=True, verbose=1)
+model_dir='trained_models/'
+if not os.path.isdir(model_dir): os.system('mkdir '+model_dir)
+model0.save(model_dir+'logi.h5')
+model1.save(model_dir+'mlp.h5')
+model_cnn.save(model_dir+'cnn.h5')
+np.savez(model_dir+'training_histories.npz', [ history.history for history in [history_logi, history_mlp, history_cnn ]])
