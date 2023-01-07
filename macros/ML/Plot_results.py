1c84804d32c534922792294ceb785097fa3639a5
DongJo Kim <dong.jo.kim@cern.ch>
2023-01-07 15:55:48 +0200

    update with result drawing

---
+++ macros/ML/Plot_results.py
@@ -0,0 +1,46 @@
+import sys, os
+import numpy as np
+# define the function
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import keras
+# save np.load
+np_load_old = np.load
+
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+model_dir='trained_models/'
+
+history_logi, history_mlp, history_cnn = np.load(model_dir+'training_histories.npz')['arr_0']
+model0 = keras.models.load_model(model_dir+'logi.h5')
+model1 = keras.models.load_model(model_dir+'mlp.h5')
+model_cnn = keras.models.load_model(model_dir+'cnn.h5')
+print(history_logi)
+outdir = 'images_out/'
+data = np.load(outdir+'allimages.npz')['arr_0']
+x_test = data[0]
+y_test = data[1]
+print(x_test.shape)
+predictions0 = model0.predict(x_test)
+predictions1 = model1.predict(x_test)
+predictions_cnn = model_cnn.predict(x_test)
+
+fig, axes = plt.subplots(1,2,figsize=(10,4))
+
+nn=20
+#axes[1].scatter(nn, 1.*sum(np.argmax(predictions0, axis=1) == np.argmax(y_test, axis=1))/ len(y_test),c='b')
+#axes[1].scatter(nn, 1.*sum(np.argmax(predictions1, axis=1) == np.argmax(y_test, axis=1))/ len(y_test),c='y')
+#axes[1].scatter(nn, 1.*sum(np.argmax(predictions_cnn, axis=1) == np.argmax(y_test, axis=1))/ len(y_test),c='g')
+for i, history in enumerate([history_logi, history_mlp, history_cnn]):
+	axes[1].plot(history['accuracy'][:nn], c=plt.get_cmap("tab10")(i), label='train')
+	axes[1].plot(history['val_accuracy'][:nn], c=plt.get_cmap("tab10")(i), ls ='--', label='validation')
+	axes[0].plot(history['loss'][:nn], c=plt.get_cmap("tab10")(i), label=['logistic','small MLP', 'CNN', 'CNN v2'][i])
+	axes[0].plot(history['val_loss'][:nn], c=plt.get_cmap("tab10")(i), ls='--')
+for iax in range(2):
+	axes[iax].set(xlabel='epoch', ylabel=['loss','accuracy'][iax])
+
+axes[0].legend()
+axes[1].legend(['train','validation'])
+fig.show()
+fig.savefig('figs/training_history.png')
